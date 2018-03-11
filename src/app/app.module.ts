4b5ebb81131ca8bcd251664c2b3094ca6c444420
Eric <e.vic22@hotmail.com>
2018-03-10 20:00:20 -0600

    update home page with nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { BuildingsComponent } from './buildings/buildings.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuildingsComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
